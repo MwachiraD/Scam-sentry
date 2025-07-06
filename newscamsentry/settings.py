2579e841735ce4ba59dbd1c8e7b20f97e9cf19a2
Dennis <denochira78@gmail.com>
2025-07-06 12:16:06 +0300

    Temporarily allow all hosts to debug 500 error

--- newscamsentry/settings.py
+++ newscamsentry/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ngrok-free.app' , 'scamsentry.onrender.com', 'scam-sentry-ke.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
